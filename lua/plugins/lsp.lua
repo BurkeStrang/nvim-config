d1c1d93879071e879c7855249c31c52a5736df59
Burke Strang <Burke.Strang@vertigis.com>
2024-08-16 08:29:27 -0400

    update json lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -58,6 +58,7 @@ return {
       ---@type lspconfig.options
       servers = {
         bicep = {},
+        jsonls = {},
         lua_ls = {
           -- mason = false, -- set to false if you don't want this server to be installed with mason
           -- Use this to add any additional keymaps
