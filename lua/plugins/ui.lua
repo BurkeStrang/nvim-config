041591eaacb76faf1ef81b3180264e99f9a644e3
BurkeStrang <burke.strang@outlook.com>
2024-11-10 11:14:11 -0500

    remove git info will figure out later

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -49,47 +49,17 @@ return {
         },
         hide = { cursorline = true },
         render = function(props)
+          -- Get the filename and indicate if the buffer is modified
           local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
           if vim.bo[props.buf].modified then
             filename = "[*]" .. filename
           end
 
+          -- Get the icon and color for the file type
           local icon, color = require("nvim-web-devicons").get_icon_color(filename)
 
-          -- Check if inside a git repo and cache the result
-          local is_git_repo = vim.b[props.buf].is_git_repo
-          if is_git_repo == nil then
-            is_git_repo = vim.fn.systemlist("git rev-parse --is-inside-work-tree")[1] == "true"
-            vim.b[props.buf].is_git_repo = is_git_repo
-          end
-          if not is_git_repo then
-            return { { icon, guifg = color }, { " " }, { filename } }
-          end
-
-          -- Cache branch name and remote URL for each buffer to avoid repeated calls
-          local branch = vim.b[props.buf].branch
-          if not branch then
-            branch = vim.fn.systemlist("git rev-parse --abbrev-ref HEAD")[1] or "unknown"
-            vim.b[props.buf].branch = branch
-          end
-
-          local remote_url = vim.b[props.buf].remote_url
-          if not remote_url then
-            remote_url = vim.fn.systemlist("git remote get-url origin")[1] or ""
-            vim.b[props.buf].remote_url = remote_url
-          end
-
-          -- Determine repository symbol based on remote URL
-          local repo_symbol = " "
-          if remote_url:find("github.com") then
-            repo_symbol = " " -- GitHub symbol
-          elseif remote_url:find("dev.azure.com") then
-            repo_symbol = " " -- Azure DevOps symbol
-          end
-
+          -- Display only the icon and filename
           return {
-            { repo_symbol .. branch, guifg = "#AA1188" },
-            { "  " },
             { icon, guifg = color },
             { " " },
             { filename },
