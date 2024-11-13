b7475e1fb6abb8f51a1af896bcdad5c5f0667285
Burke Strang <Burke.Strang@vertigis.com>
2024-11-13 09:59:21 -0500

    update

--- lua/plugins/minimove.lua
+++ lua/plugins/minimove.lua
@@ -1,17 +1,17 @@
 return {
   "echasnovski/mini.move",
   event = "VeryLazy",
-  -- opts = {
-  --   mappings = {
-  --     left = "H",
-  --     right = "L",
-  --     down = "J",
-  --     up = "K",
-  --
-  --     line_left = "",
-  --     line_right = "",
-  --     line_down = "",
-  --     line_up = "",
-  --   },
-  -- },
+  opts = {
+    mappings = {
+      left = "<C-h>",
+      right = "<C-l>",
+      down = "<C-j>",
+      up = "<C-k>",
+
+      -- line_left = "<C-h>",
+      -- line_right = "<C-l>",
+      -- line_down = "<C-j>",
+      -- line_up = "<C-k>",
+    },
+  },
 }
